3c29d140f4ab89e7a675e69ee5094254f93647ec
Frank McCallion <fkmccallion@gmail.com>
2018-04-06 02:29:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       end
     elsif req.path.match(/add/)
       search = req.params["item"]
-      binding.pry
+      #binding.pry
       if @@items.include?(search)
         @@cart << search
         resp.write "added #{search}"
